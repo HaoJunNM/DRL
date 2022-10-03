7796d91c2379abf27d8e5be8e4917f59fd966945
HaoJunNM <junhaonm@hotmail.com>
2022-10-03 12:41:49 -0600

    Add files via upload

--- Env_v1.py
+++ Env_v1.py
@@ -82,3 +82,20 @@ class BiasEnv_v1(Env):
         done = False
         observation = self._get_obs
         return observation
+
+if __name__ == '__main__':
+    env = BiasEnv_v1()
+    print(env.action_space.sample())
+    print(env.observation_space.sample())
+
+    episodes = 10
+    for episode in range(1, episodes+1):
+        state = env.reset()
+        done = False
+        score = 0
+        
+        while not done:
+            action = env.action_space.sample()
+            n_state, reward, done, info = env.step(action)
+            score += reward
+        print("Episode:{} Score:{}".format(episode, score))
